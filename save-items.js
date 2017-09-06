3f09b4f951133a55ead9c151888a88441123e13d
Michalik, Joshua <jsmichal@usa.wtbts.net>
2017-09-06 15:23:36 -0400

    Change save items script to save schedule checkpoints

--- save-items.js
+++ save-items.js
@@ -12,50 +12,50 @@ const datastore = Datastore({
 // The Cloud Datastore key for the new entity
 const key = datastore.key(['BibleBook', datastore.int('5629499534213120')]);
 
-var books = [
-   { name: 'Matthew', chs: 28 },
-   { name: 'Mark', chs: 16 },
-   { name: 'Luke', chs: 24 },
-   { name: 'John', chs: 21 },
-   { name: 'Acts', chs: 28 },
-   { name: 'Romans', chs: 16 },
-   { name: '1 Corinthians', chs: 16 },
-   { name: '2 Corinthians', chs: 13 },
-   { name: 'Galatians', chs: 6 },
-   { name: 'Ephesians', chs: 6 },
-   { name: 'Philippians', chs: 4 },
-   { name: 'Colossians', chs: 4 },
-   { name: '1 Thessalonians', chs: 5 },
-   { name: '2 Thessalonians', chs: 3 },
-   { name: '1 Timothy', chs: 6 },
-   { name: '2 Timothy', chs: 4 },
-   { name: 'Titus', chs: 3 },
-   { name: 'Hebrews', chs: 13 },
-   { name: 'Philemon', chs: 1 },
-   { name: 'James', chs: 5 },
-   { name: '1 Peter', chs: 5 },
-   { name: '2 Peter', chs: 3 },
-   { name: '1 John', chs: 5 },
-   { name: '2 John', chs: 1 },
-   { name: '3 John', chs: 1 },
-   { name: 'Jude', chs: 1 },
-   { name: 'Revelation', chs: 22 },
+var checkpoints = [
+  {name: "Matthew 21:19-25:46",   parent: 4, schedule: 3},
+  {name: "Mark 11:20-13:37",      parent: 4, schedule: 3},
+  {name: "Luke 20:1-21:38",       parent: 4, schedule: 3},
+  {name: "Matthew 26:1-5, 14-16", parent: 5, schedule: 3},
+  {name: "Mark 14:1, 2, 10, 11",  parent: 5, schedule: 3},
+  {name: "Luke 22:1-6",           parent: 5, schedule: 3},
+  {name: "Matthew 26:17-19",      parent: 6, schedule: 3},
+  {name: "Mark 14:12-16",         parent: 6, schedule: 3},
+  {name: "Luke 22:7-13",          parent: 6, schedule: 3},
+  {name: "Matthew 26:20-35",      parent: 7, schedule: 3},
+  {name: "Mark 14:17-31",         parent: 7, schedule: 3},
+  {name: "Luke 22:14-38",         parent: 7, schedule: 3},
+  {name: "John 13:1-17:26",       parent: 7, schedule: 3},
+  {name: "Matthew 26:36-75",      parent: 7, schedule: 3},
+  {name: "Mark 14:32-72",         parent: 7, schedule: 3},
+  {name: "Luke 22:39-65",         parent: 7, schedule: 3},
+  {name: "John 18:1-27",          parent: 7, schedule: 3},
+  {name: "Matthew 27:1-61",       parent: 7, schedule: 3},
+  {name: "Mark 15:1-47",          parent: 7, schedule: 3},
+  {name: "Luke 22:66-23:56",      parent: 7, schedule: 3},
+  {name: "John 18:28-19:42",      parent: 7, schedule: 3},
+  {name: "Matthew 27:62-66",      parent: 8, schedule: 3},
+  {name: "Mark 16:1",             parent: 9, schedule: 3},
+  {name: "Matthew 28:1-15",       parent: 9, schedule: 3},
+  {name: "Mark 16:2-8",           parent: 9, schedule: 3},
+  {name: "Luke 24:1-49",          parent: 9, schedule: 3},
+  {name: "John 20:1-25",          parent: 9, schedule: 3}
 ];
 
-var BOOK_NUM = 40;
-var KIND = 'BibleBook';
-books.forEach(function(book) {
+var CP_NUM = 22;
+var KIND = 'ScheduleCheckpoint';
+checkpoints.forEach(function(cp) {
    var saveData = {
       key: datastore.key(KIND),
       data: {
-         WrittenLanguage: 'Greek',
-         BookOrderNumber: BOOK_NUM,
-         ChapterCount: book.chs,
-         BookName: book.name
+         CheckpointID: CP_NUM,
+         CheckpointName: cp.name,
+         ScheduleID: cp.schedule,
+         ParentID: cp.parent
       }
    }
    datastore.save(saveData, function(err) {
-      console.log("Saved", book.name);
+      console.log("Saved", cp.name);
    });
-   BOOK_NUM++;
+   CP_NUM++;
 });
