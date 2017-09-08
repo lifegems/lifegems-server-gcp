5dd726852db29b9f0bb734a26971f741237d3411
Michalik, Joshua <jsmichal@usa.wtbts.net>
2017-09-08 16:46:18 -0400

    Rework schedules api

--- routes/schedules.js
+++ routes/schedules.js
@@ -1,35 +1,64 @@
 var datastore = require('../config/datastore');
 var router = require('express').Router();
 
-router.get('/schedules', function(req, res) {
+router.get('/schedules', (req, res) => {
    console.log('  GET 200', req.originalUrl);
    const query = datastore.createQuery('Schedule').limit(100).order('ScheduleID');
-   datastore.runQuery(query, function(err, rows) {
-      var schedules = rows.map(function(row) {
+   datastore.runQuery(query, (err, rows) => {
+      var schedules = rows.map((row) => {
          return {
             id: row.ScheduleID,
-            name: row.ScheduleName
+            code: row.ScheduleCode,
+            name: row.ScheduleName,
+            version: row.Version,
          }
       });
       res.send(schedules);
    });
 });
 
-router.get('/schedules/:id', function(req, res) {
+router.get('/schedules/:id', (req, res) => {
    console.log('  GET 200', req.originalUrl);
-   const id = req.params.id;
+   const query = datastore.createQuery('Schedule').filter('ScheduleID', parseInt(req.params.id)).limit(1);
+   datastore.runQuery(query, (err, rows) => {
+      var schedule = {
+         id: rows[0].ScheduleID,
+         code: rows[0].ScheduleCode,
+         name: rows[0].ScheduleName,
+         version: rows[0].Version,
+      };
+      res.send(schedule);
+   });
+});
+
+router.get('/checkpoints', function(req, res) {
+   const id = req.query.scheduleId;
+   const parent = req.query.parent;
+   if (!id) {
+      console.log('  GET 400', req.originalUrl, "No schedule specified.");
+      res.status(400).send('Invalid Request');
+      return false;
+   }
+   console.log('  GET 200', req.originalUrl);
    const query = datastore.createQuery('ScheduleCheckpoint').filter('ScheduleID', parseInt(id));
    datastore.runQuery(query, function(err, rows) {
       rows = transformQuery(rows);
-      var sections = nestSections(rows.filter(r => r.parent == 0), rows);
+      var checkpointLevel = getSectionCheckpointLevel(rows);
+      var startSections = (parent) ? rows.filter(r => r.parent == parent) : rows.filter(r => (r.code.match(/\./g) || []).length == checkpointLevel);
+      var sections = nestSections(startSections, rows);
       sections.forEach(s => s.sections = nestSections(s.sections, rows));
       res.send(sections);
    });
 });
 
+function getSectionCheckpointLevel(sections) {
+   var levels = sections.map(s => (s.code.match(/\./g) || []).length)
+   return levels.reduce((a, b) => Math.min(a,b));
+}
+
 function nestSections(sections, allRows) {
    return sections.map(s => Object.assign(s, {
-      sections: allRows.filter(r => r.parent == s.id)
+      sections: allRows.filter(r => r.parent == s.code)
    }));
 }
 
@@ -39,8 +68,9 @@ function transformQuery(rows) {
       .map(r => {
          return {
             id: r.CheckpointID,
+            code: r.CheckpointCode,
             name: r.CheckpointName,
-            parent: r.ParentID,
+            parent: r.ParentCheckpointCode,
             schedule: r.ScheduleID,
             sections: []
          }
